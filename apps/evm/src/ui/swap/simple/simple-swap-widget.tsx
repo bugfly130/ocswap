522ae084de48d850e5df24d5776c57b83abcbd92
newstar121 <bg.newstar121@gmail.com>
2024-04-09 14:32:11 +0000

    a

--- apps/evm/src/ui/swap/simple/simple-swap-widget.tsx
+++ apps/evm/src/ui/swap/simple/simple-swap-widget.tsx
@@ -16,47 +16,58 @@ import React, { useMemo } from 'react'
 import { formatUSD } from 'sushi/format'
 import { useDerivedStateSimpleSwap } from './derivedstate-simple-swap-provider'
 
-import { type GetPoolsArgs } from '@sushiswap/client'
+import { type GetPoolsArgs, getPools } from '@sushiswap/client'
 import { usePools } from '@sushiswap/client/hooks'
+import { Native } from 'sushi/currency'
 
 export const SimpleSwapWidget = () => {
-  // const {
-  //   state: { token0, token1, chainId },
-  // } = useDerivedStateSimpleSwap()
+  const {
+    state: { token0, token1, chainId },
+  } = useDerivedStateSimpleSwap()
 
-  // const args = useMemo<GetPoolsArgs>(() => {
-  //   console.log('aaaaaaaaaaaa', chainId, token0, token1)
-  //   if (token0?.symbol && token1?.symbol)
-  //     return {
-  //       chainIds: [chainId],
-  //       tokenSymbols: [token0?.symbol, token1?.symbol],
-  //       isWhitelisted: true,
-  //     }
-  //   else return undefined
-  // }, [token0, token1, chainId])
+  const args = useMemo<GetPoolsArgs>(() => {
+    if (token0?.symbol && token1?.symbol)
+      return {
+        chainIds: [chainId],
+        tokenSymbols: [token0?.symbol, token1?.symbol],
+        isWhitelisted: true,
+      }
+    else return undefined
+  }, [token0, token1, chainId])
 
-  // const { data: pools } = usePools({ args, shouldFetch: true })
-  // console.log('aaaaaaaaaaab', pools)
-  // const { pool } = useMemo(() => {
-  //   return {
-  //     pool:
-  //       pools
-  //         ?.flat()
-  //         .filter(
-  //           (pool) =>
-  //             (pool.token0.symbol === token0?.symbol &&
-  //               pool.token1.symbol === token1?.symbol) ||
-  //             (pool.token1.symbol === token0?.symbol &&
-  //               pool.token0.symbol === token1?.symbol),
-  //         )
-  //         .at(0) || undefined,
-  //   }
-  // }, [pools, token0, token1])
-  // console.log('aaaaaaaaaaac', pool)
+  const { data: pools } = usePools({ args, shouldFetch: true })
+
+  const pool = useMemo(() => {
+    const foundP = pools?.flat().find((p) => {
+      const [_token0, _token1] = [
+        Native.onChain(chainId).wrapped.address.toLowerCase() ===
+        p.token0.address.toLowerCase()
+          ? Native.onChain(chainId)
+          : p.token0,
+        Native.onChain(chainId).wrapped.address.toLowerCase() ===
+        p.token1.address.toLowerCase()
+          ? Native.onChain(chainId)
+          : p.token1,
+      ]
+      if (
+        (token0?.symbol === _token0.symbol &&
+          token1?.symbol === _token1.symbol) ||
+        (token1?.symbol === _token0.symbol && token0?.symbol === _token1.symbol)
+      ) {
+        return true // Return true to include the current element in the foundP result
+      }
+    })
+
+    return foundP
+  }, [pools, token0, token1, chainId])
 
   return (
     <div className="flex flex-col gap-4">
       <SimpleSwapHeader />
+      <span className="text-slate-50">
+        {pool?.name} Volume:
+        {pool?.volumeUSD ? formatUSD(pool?.volume1d) : 'N/A'}
+      </span>
       <div className="flex items-center justify-between">
         <SwapModeButtons />
         <SimpleSwapSettingsOverlay />
