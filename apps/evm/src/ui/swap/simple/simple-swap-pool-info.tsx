33712fd0bd7724f600f5a2ede6c201fdc5040382
newstar121 <bg.newstar121@gmail.com>
2024-04-11 18:13:07 +0000

    a

--- apps/evm/src/ui/swap/simple/simple-swap-pool-info.tsx
+++ apps/evm/src/ui/swap/simple/simple-swap-pool-info.tsx
@@ -8,7 +8,10 @@ import { formatUSD } from 'sushi/format'
 import { type GetPoolsArgs } from '@sushiswap/client'
 import { usePools } from '@sushiswap/client/hooks'
 import { Currency } from '@sushiswap/ui/components/currency'
+import { unwrapToken } from 'src/lib/functions'
 import { Native, Token } from 'sushi/currency'
+
+import { MarketCapIcon } from '@sushiswap/ui/components/icons'
 
 export const SimpleSwapPoolInfo = () => {
   const {
@@ -52,6 +55,31 @@ export const SimpleSwapPoolInfo = () => {
     return foundP
   }, [pools, token0, token1, chainId])
 
+  const [token0_, token1_] = useMemo(() => {
+    if (!pool) return [undefined, undefined]
+
+    return [
+      unwrapToken(
+        new Token({
+          chainId: pool.chainId,
+          address: pool.token0.address,
+          decimals: pool.token0.decimals,
+          symbol: pool.token0.symbol,
+        }),
+      ),
+      unwrapToken(
+        new Token({
+          chainId: pool.chainId,
+          address: pool.token1.address,
+          decimals: pool.token1.decimals,
+          symbol: pool.token1.symbol,
+        }),
+      ),
+    ]
+  }, [pool])
+
+  console.log('aaaaaaaa', pool)
+
   return (
     <div className="flex flex-col">
       <div className="flex flex-row gap-6">
@@ -62,37 +90,37 @@ export const SimpleSwapPoolInfo = () => {
             backdropFilter: 'blur(35px)',
           }}
         >
-          {!isLoading ? (
-            <div className="gap-4">
-              <div className="flex gap-2">
-                {/* <Currency.IconList iconWidth={26} iconHeight={26}>
-                  {pool?.token0 ? (
-                    <Currency.Icon currency={pool?.token0} />
-                  ) : null}
-                  {token1 ? <Currency.Icon currency={token1} /> : null}
-                </Currency.IconList> */}
+          {!isLoading && pool && token0_ && token1_ ? (
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">
+                <Currency.IconList iconWidth={26} iconHeight={26}>
+                  <Currency.Icon currency={token0_} />
+                  <Currency.Icon currency={token1_} />
+                </Currency.IconList>
                 <span className="text-sm text-gray-400">
                   {token0?.symbol}/{token1?.symbol}
                 </span>
               </div>
               <div className="flex gap-2">
                 <span className="text-lg text-slate-50">
-                  {pool?.volumeUSD ? formatUSD(pool?.volume1d) : 'N/A'}
+                  {formatUSD(pool.volume1d)}
                 </span>
               </div>
             </div>
           ) : (
-            <div>a</div>
+            ''
           )}
         </div>
         <div
-          className="flex flex-col items-center flex-1 gap-6 p-6 rounded-xl"
+          className="flex flex-col items-center flex-1 gap-2 p-4 rounded-xl"
           style={{
             background: 'rgba(255, 255, 255, 0.08)',
             backdropFilter: 'blur(35px)',
           }}
         >
-          a
+          <MarketCapIcon width={32} height={32} />
+          <span className="text-lg text-slate-50">a</span>
+          <span className="text-sm text-gray-400">Market Cap</span>
         </div>
         <div
           className="flex flex-col items-center flex-1 gap-6 p-6 rounded-xl"
